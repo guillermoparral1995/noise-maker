9fb35996159989aa3dbd790e44f590c5c31d7868
Guillermo Parral <guillermoeparral@gmail.com>
2023-12-30 20:14:16 +0100

    change webpack output to docs so it can be read by github pages

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const path = require('path');
 module.exports = {
   mode: 'development',
   entry: './src/index.tsx',
-  output: { path: path.resolve(__dirname, 'dist') },
+  output: { path: path.resolve(__dirname, 'docs') },
   devtool: 'source-map',
   module: {
     rules: [
