f60c4e8b8c53b77dea29a4641d79dc6ad51605d7
Guillermo Parral <guillermoeparral@gmail.com>
2024-01-20 21:16:01 +0100

    adjust vite config for correctly serving assets

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,16 @@
+/** @type {import('vite').UserConfig} */
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
   root: './src',
   build: {
-    outDir: '../docs/noise-maker/',
+    emptyOutDir: true,
+    outDir: '../docs',
+    assetsDir: 'noise-maker/assets',
   },
   publicDir: '../public',
+
   plugins: [
     react({
       include: '**/*.{tsx}',
