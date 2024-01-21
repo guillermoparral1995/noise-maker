21a0234888f49dc7edb36a71c5e666690a25050c
Guillermo Parral <guillermoeparral@gmail.com>
2024-01-21 10:46:30 +0100

    add service worker for caching assets

---
+++ docs/service-worker.js
@@ -0,0 +1,47 @@
+const putInCache = async (request, response) => {
+  const cache = await caches.open('v1');
+  await cache.put(request, response);
+};
+
+const cacheFirst = async ({ request, preloadResponsePromise }) => {
+  const responseFromCache = await caches.match(request);
+  if (responseFromCache) {
+    return responseFromCache;
+  }
+
+  const preloadResponse = await preloadResponsePromise;
+  if (preloadResponse) {
+    putInCache(request, preloadResponse.clone());
+    return preloadResponse;
+  }
+
+  try {
+    const responseFromNetwork = await fetch(request.clone());
+    putInCache(request, responseFromNetwork.clone());
+    return responseFromNetwork;
+  } catch (error) {
+    return new Response('Network error happened', {
+      status: 408,
+      headers: { 'Content-Type': 'text/plain' },
+    });
+  }
+};
+
+const enableNavigationPreload = async () => {
+  if (self.registration.navigationPreload) {
+    await self.registration.navigationPreload.enable();
+  }
+};
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(enableNavigationPreload());
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    cacheFirst({
+      request: event.request,
+      preloadResponsePromise: event.preloadResponse,
+    }),
+  );
+});
--- src/index.tsx
+++ src/index.tsx
@@ -9,28 +9,44 @@ import 'primeicons/primeicons.css';
 import 'primereact/resources/themes/lara-dark-purple/theme.css';
 import 'primereact/resources/themes/lara-light-purple/theme.css';
 
-navigator.mediaDevices.getUserMedia({ audio: true }).then(() => {
-  const appContainer = document.createElement('div');
-  appContainer.id = 'root';
-  document.querySelector('body').appendChild(appContainer);
-  const root = createRoot(appContainer);
-
-  root.render(
-    <>
-      <Helmet>
-        <link
-          id="theme-link"
-          rel="stylesheet"
-          href={'themes/lara-light-purple/theme.css'}
-        />
-      </Helmet>
-      <PrimeReactProvider>
-        <AudioContextProvider>
-          <MIDIProvider>
-            <App />
-          </MIDIProvider>
-        </AudioContextProvider>
-      </PrimeReactProvider>
-    </>,
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('/service-worker.js').then(
+    (registration) => {
+      console.log('Service worker registration succeeded:', registration);
+    },
+    (error) => {
+      console.error(`Service worker registration failed: ${error}`);
+    },
   );
-});
+} else {
+  console.error('Service workers are not supported.');
+}
+
+navigator.mediaDevices
+  .getUserMedia({ audio: true })
+  .then(() => {
+    const appContainer = document.createElement('div');
+    appContainer.id = 'root';
+    document.querySelector('body').appendChild(appContainer);
+    const root = createRoot(appContainer);
+
+    root.render(
+      <>
+        <Helmet>
+          <link
+            id="theme-link"
+            rel="stylesheet"
+            href={'themes/lara-light-purple/theme.css'}
+          />
+        </Helmet>
+        <PrimeReactProvider>
+          <AudioContextProvider>
+            <MIDIProvider>
+              <App />
+            </MIDIProvider>
+          </AudioContextProvider>
+        </PrimeReactProvider>
+      </>,
+    );
+  })
+  .catch((error) => console.error('Audio devices are not supported: ', error));
